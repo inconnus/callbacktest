f9f4ac5ddd4cff2e88b84369203b32f448519b80
inconnus <chindanai.mal@gmail.com>
2022-05-20 13:48:28 +0700

    router.query.status

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
-
+import { useRouter } from 'next/router'
 const Home: NextPage = () => {
+  const router = useRouter()
   useEffect(() => {
-    window.parent.postMessage("sus", "*")
-  }, [])
+    if (!router.query.status) return
+    window.parent.postMessage(router.query.status || 'empty', "*")
+  }, [router.query.status])
   return (
     <div className={styles.container}>
       test
